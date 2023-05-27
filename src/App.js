40de4083ae703a5775b4edd112760672c9b8ee3b
roamingterra <danielballerini1@gmail.com>
2023-05-27 17:08:52 -0400

    Create basic app layout
    I set up the react app and styled the main components of the app. I
    set up the card component, which will be the main component, duplicated
    12 times, that will act as random images for the user to select.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import Card from "./components/card";
+import "./styles/style.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="header">
+        <div className="title">Memory Card Game</div>
+        <div className="instructions">
+          Get points by clicking on the an image, don't click on an image more
+          than once
+        </div>
+        <div className="score-board">
+          <div>Score:</div>
+          <div>Best Score:</div>
+        </div>
+      </div>
+      <div className="card-section-container">
+        <div className="card-section">
+          <Card></Card>
+          <Card></Card>
+          <Card></Card>
+          <Card></Card>
+          <Card></Card>
+          <Card></Card>
+          <Card></Card>
+          <Card></Card>
+          <Card></Card>
+          <Card></Card>
+          <Card></Card>
+          <Card></Card>
+        </div>
+      </div>
+      <div className="footer">
+        <div>Website created by Daniel Ballerini</div>
+        <div>
+          with help from the
+          <a href="https://www.theodinproject.com/"> © Odin Project</a>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/card.js
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from "react";
+
+function Card(props) {
+  return (
+    <div className="card">
+      <div className="img"></div>
+    </div>
+  );
+}
+
+export default Card;
